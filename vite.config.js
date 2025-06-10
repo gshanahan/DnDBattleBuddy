f7f83571b760cde2f8ac0e3a241ab5cdd8133642
gabriel-shanahan <41175126+gshanahan@users.noreply.github.com>
2025-06-10 11:16:38 -0700

    Setting up GitHub pages hosting

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,12 @@
-// vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/DnDBattleBuddy/',
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
 });
-
