e368391de517e28702890992a050d5e7ff34780f
gabriel-shanahan <41175126+gshanahan@users.noreply.github.com>
2025-06-10 11:41:03 -0700

    Updated vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 import path from 'path';
 
 export default defineConfig({
+  base: '/DnDBattleBuddy/',  // <-- add this line
   plugins: [react()],
   resolve: {
     alias: {
